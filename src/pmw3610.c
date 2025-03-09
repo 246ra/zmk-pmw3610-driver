e8a6f10647d3d039dc24bcf2caf86cb55ac60e0c
nishimura <33683812+246ra@users.noreply.github.com>
2025-03-09 15:34:47 +0900

    Update pmw3610.c
    修正

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -723,7 +723,9 @@ static int pmw3610_report_data(const struct device *dev) {
 #endif
 
     // ボールアクション初期化
-    is_ball_action = true;
+    if (!is_ball_action) {
+        is_ball_action = true;
+    }
     
     if (x != 0 || y != 0) {
         if (input_mode == MOVE || input_mode == SNIPE) {
@@ -802,10 +804,8 @@ static int pmw3610_report_data(const struct device *dev) {
         }
     } else {
         //if (!is_ball_action && k_uptime_get() - curr_ball_time > ball_action_delta_time) {
-        //if (!is_ball_action) {
         //    is_ball_action = true;
         //}
-        is_ball_action = true;
     }
 
     return err;
