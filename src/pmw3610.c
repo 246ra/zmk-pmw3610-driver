5b4d4bd1f3c7e3e4fe1a003ef73d32d19111a30a
nishimura <33683812+246ra@users.noreply.github.com>
2025-03-15 13:08:53 +0900

    Update pmw3610.c
    追加

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -801,7 +801,7 @@ static int pmw3610_report_data(const struct device *dev) {
                 //    }
                 //}
                 int idx = -1;
-                float r = my_sqrt((data->ball_action_delta_x * data->ball_action_delta_x) + (data->ball_action_delta_y * data->ball_action_delta_y))
+                float r = my_sqrt((data->ball_action_delta_x * data->ball_action_delta_x) + (data->ball_action_delta_y * data->ball_action_delta_y));
                 //ボールの動きを判定（左上0、右上1、左2、右3、右下4、左下5）
                 if (r < action_cfg.tick) {
                     float y_30 = abs(data->ball_action_delta_x) * theta_30;
